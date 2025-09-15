9f717ab82e677b203aae4f4aa7e29c2156ff6ffb
Dan Salberg <daniel.j.salberg@gmail.com>
2025-09-15 17:14:22 -0500

    create first template for serving dynamic content (todos)

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the ToDos index.")
+# def index(request):
+#     return HttpResponse("Hello world, this is your todo index.")
+
+def todo_list(request):
+    return render(request, 'todo/todo_list.html', {})
