48cf58e329cf58d8a395890d6a9a4ee2428ed4e3
Dan Salberg <daniel.j.salberg@gmail.com>
2025-09-15 16:32:50 -0500

    create new todo model and instantiate new instance for testing

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ToDo(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
