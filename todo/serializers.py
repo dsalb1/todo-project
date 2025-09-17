b76bb791a1f4a5db2a7f9b3dedbc376bd4498c32
Dan Salberg <daniel.j.salberg@gmail.com>
2025-09-17 14:01:59 -0500

    commit the serializers file as well

---
+++ todo/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from todo.models import ToDo
+
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    todos = serializers.HyperlinkedRelatedField(many=True, view_name='todo:todo-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'todos']
+
+
+class ToDoSerializer(serializers.HyperlinkedModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    
+    class Meta:
+        model = ToDo
+        fields = ['id', 'title', 'text', 'author', 'created_date', 'updated_date']
